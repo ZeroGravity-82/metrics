79e7ea64105a4b3a2b6f909e5d9f3900361ea921
Nikolay Ryabkov <n.riabkov@mts-link.ru>
2025-10-27 17:06:18 +0700

    iter4: добавил схему в дефолтный адрес сервера

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@ type Config struct {
 
 func ParseFlags() Config {
 	config := Config{}
-	config.ServerAddr = flag.String("a", "localhost:8080", "адрес HTTP-сервера")
+	config.ServerAddr = flag.String("a", "http://localhost:8080", "адрес HTTP-сервера")
 	config.ReportInterval = flag.Int("r", 10, "частота отправки метрик на сервер")
 	config.PollInterval = flag.Int("p", 2, "частота опроса метрик из пакета runtime")
 	flag.Parse()
