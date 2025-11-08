39b532311cfa56525a48db14b638cb10094d957f
Nikolay Ryabkov <n.riabkov@mts-link.ru>
2025-11-08 13:33:28 +0700

    iter6: добавил для логов значение по умолчанию для кода статуса ответа

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -42,7 +42,7 @@ func withLogging(next http.Handler) http.Handler {
 		start := time.Now()
 
 		responseData := &responseData{
-			status: 0,
+			status: http.StatusOK,
 			size:   0,
 		}
 		lw := loggingResponseWriter{
