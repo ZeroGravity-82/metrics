396cfe01a914c79fddf465dbf5a88581ec970db6
Nikolay Ryabkov <n.riabkov@mts-link.ru>
2025-11-23 20:54:24 +0700

    iter9: дополнил тесты конфигурации сервера

--- internal/config/config_server_test.go
+++ internal/config/config_server_test.go
@@ -16,6 +16,12 @@ func TestGetServerConfig_Default(t *testing.T) {
 	os.Args = []string{"server"}
 	err := os.Unsetenv("ADDRESS")
 	require.NoError(t, err)
+	err = os.Unsetenv("STORE_INTERVAL")
+	require.NoError(t, err)
+	err = os.Unsetenv("FILE_STORAGE_PATH")
+	require.NoError(t, err)
+	err = os.Unsetenv("RESTORE")
+	require.NoError(t, err)
 
 	// Act
 	cfg, err := GetServerConfig()
@@ -23,14 +29,23 @@ func TestGetServerConfig_Default(t *testing.T) {
 	// Assert
 	require.NoError(t, err)
 	assert.Equal(t, defaultServerAddr, cfg.ServerAddr)
+	assert.Equal(t, defaultStoreInterval, cfg.StoreInterval)
+	assert.Equal(t, defaultFileStoragePath, cfg.FileStoragePath)
+	assert.Equal(t, defaultRestore, cfg.Restore)
 }
 
 // TestGetServerConfig_Flag проверяет парсинг параметров командной строки сервера
 func TestGetServerConfig_Flag(t *testing.T) {
 	// Arrange
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	os.Args = []string{"server", "-a=127.0.0.1:8081"}
+	os.Args = []string{"server", "-a=127.0.0.1:8081", "-i=200", "-f=metrics.json", "-r"}
 	err := os.Unsetenv("ADDRESS")
+	require.NoError(t, err)
+	err = os.Unsetenv("STORE_INTERVAL")
+	require.NoError(t, err)
+	err = os.Unsetenv("FILE_STORAGE_PATH")
+	require.NoError(t, err)
+	err = os.Unsetenv("RESTORE")
 	require.NoError(t, err)
 
 	// Act
@@ -39,6 +54,9 @@ func TestGetServerConfig_Flag(t *testing.T) {
 	// Assert
 	require.NoError(t, err)
 	assert.Equal(t, "127.0.0.1:8081", cfg.ServerAddr)
+	assert.Equal(t, 200, cfg.StoreInterval)
+	assert.Equal(t, "metrics.json", cfg.FileStoragePath)
+	assert.Equal(t, true, cfg.Restore)
 }
 
 // TestGetServerConfig_Env проверяет парсинг переменных окружения сервера
@@ -46,23 +64,13 @@ func TestGetServerConfig_Env(t *testing.T) {
 	// Arrange
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	os.Args = []string{"server"}
-	err := os.Setenv("ADDRESS", "127.0.0.1:8081")
-	require.NoError(t, err)
-
-	// Act
-	cfg, err := GetServerConfig()
-
-	// Assert
-	require.NoError(t, err)
-	assert.Equal(t, "127.0.0.1:8081", cfg.ServerAddr)
-}
-
-// TestGetServerConfig_EnvPrecedence проверяет приоритет переменных окружения над параметрами командной строки сервера
-func TestGetServerConfig_EnvPrecedence(t *testing.T) {
-	// Arrange
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	os.Args = []string{"server", "-a=127.0.0.1:8081"}
 	err := os.Setenv("ADDRESS", "localhost:8085")
+	require.NoError(t, err)
+	err = os.Setenv("STORE_INTERVAL", "250")
+	require.NoError(t, err)
+	err = os.Setenv("FILE_STORAGE_PATH", "my_metric.json")
+	require.NoError(t, err)
+	err = os.Setenv("RESTORE", "false")
 	require.NoError(t, err)
 
 	// Act
@@ -71,4 +79,32 @@ func TestGetServerConfig_EnvPrecedence(t *testing.T) {
 	// Assert
 	require.NoError(t, err)
 	assert.Equal(t, "localhost:8085", cfg.ServerAddr)
+	assert.Equal(t, 250, cfg.StoreInterval)
+	assert.Equal(t, "my_metric.json", cfg.FileStoragePath)
+	assert.Equal(t, false, cfg.Restore)
+}
+
+// TestGetServerConfig_EnvPrecedence проверяет приоритет переменных окружения над параметрами командной строки сервера
+func TestGetServerConfig_EnvPrecedence(t *testing.T) {
+	// Arrange
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	os.Args = []string{"server", "-a=127.0.0.1:8081", "-i=200", "-f=metrics.json", "-r"}
+	err := os.Setenv("ADDRESS", "localhost:8085")
+	require.NoError(t, err)
+	err = os.Setenv("STORE_INTERVAL", "250")
+	require.NoError(t, err)
+	err = os.Setenv("FILE_STORAGE_PATH", "my_metric.json")
+	require.NoError(t, err)
+	err = os.Setenv("RESTORE", "false")
+	require.NoError(t, err)
+
+	// Act
+	cfg, err := GetServerConfig()
+
+	// Assert
+	require.NoError(t, err)
+	assert.Equal(t, "localhost:8085", cfg.ServerAddr)
+	assert.Equal(t, 250, cfg.StoreInterval)
+	assert.Equal(t, "my_metric.json", cfg.FileStoragePath)
+	assert.Equal(t, false, cfg.Restore)
 }
